4436e427c0fbc9a40ec6fc80bb0353220f4b8dda
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2022-10-12 12:04:55 +0300

    Update Bakkal Hesap Makinası.py

--- Bakkal Hesap Makinası.py
+++ Bakkal Hesap Makinası.py
@@ -5,58 +5,89 @@ copyright (C) 2022 BatuHanHub Software
 This is free software; you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.""")
 
+while True:
 
 ##########################################################################################################
 
 #koli miktarı
 
-Box = int(input("\nKac tane koliniz var? : ")) #kullanıcıdan koli miktarını istiyor.
+ Box = int(input("\nKac tane koliniz var? : ")) #kullanıcıdan koli miktarını istiyor.
+ 
+ if Box > 0:
+   pass
+   
+ elif Box < 0:
+   print("\nhatalı sayı gidiniz lütfen düzeltiniz\n")  
+   continue 
 
 #########################################################################################################
 
 #biri koli içindeki su
 
-BoxWater = int(input("bir kolide kaç tane su var? : ")) #1 kolideki su miktarını istiyor
+ BoxWater = int(input("bir kolide kaç tane su var? : ")) #1 kolideki su miktarını istiyor
+ 
+ if BoxWater > 0:
+   pass
+   
+ elif BoxWater < 0:
+   print("\nhatalı sayı gidiniz lütfen düzeltiniz\n")
+   continue
 
 #bütün kolideki su hesaplanıyor
 
-TotalWater = Box * BoxWater #işlem1:toplam koli sayısı ile 1 kolide olan su miktarını çarpıyor.
-print("*toplam kolilerdeki su miktarı :",TotalWater)
+ TotalWater = Box * BoxWater #işlem1:toplam koli sayısı ile 1 kolide olan su miktarını çarpıyor.
+ print("*toplam kolilerdeki su miktarı :",TotalWater)
 
 ##########################################################################################################
 
 #alışfiyatı
 
-PurchasePrice = float(input("Alısfiyatınızı giriniz: ")) #suyun alış fiyatını soruyor.
-Water = PurchasePrice / TotalWater
-print("*bir suyun alısfiyatı:", Water)
+ PurchasePrice = float(input("Alısfiyatınızı giriniz: ")) #suyun alış fiyatını soruyor.
+ 
+ if PurchasePrice > 0:
+   pass
+   
+ elif PurchasePrice < 0:
+   print("\nhatalı sayı gidiniz lütfen düzeltiniz örneğin x.y yapınız\n")   
+   continue
+
+ Water = PurchasePrice / TotalWater
+ print("*bir suyun alısfiyatı:", Water)
 
 ###########################################################################################################
 
 #Kâr marjı
 
-Gain = float(input("kâr marjınızı giriniz: ")) #sudan kazanılan parayı soruyor
+ Gain = float(input("kâr marjınızı giriniz: ")) #sudan kazanılan parayı soruyor
+
+ if Gain > 0:
+   pass
+   
+ elif Gain < 0:
+   print("\nhatalı sayı gidiniz lütfen düzeltiniz x.y yapınız\n")
+   continue
 
 ###########################################################################################################
-
+ 
 #suyun toplam satış fiyatı
-print("\n SONUCLAR")
+ print("\n SONUCLAR")
 
-Total = Water + Gain
+ Total = Water + Gain
 
-print("\nsatıs fiyatınız: ", Total)
+ print("\nsatıs fiyatınız: ", Total)
 
 #toplam kolilerdeki su fiyatı
 
-Process1 = Total * TotalWater
+ Process1 = Total * TotalWater
 
-print("Toplam kolideki su fiyatınız: ", Process1)
+ print("Toplam kolideki su fiyatınız: ", Process1)
 
 #toplam su fiyatı ile Kâr hesaplanıyor
 
-TotalGain = TotalWater * Gain
+ TotalGain = TotalWater * Gain
 
-print("toplam sudan kazandıgınız kâr: ", TotalGain)
+ print("toplam sudan kazandıgınız kâr: ", TotalGain)
+
 
 ############################################################################################################
 
